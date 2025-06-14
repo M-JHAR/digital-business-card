306c16abd1ee23c4392710265a2f0d1c9fefbcf6
Mustafa Jhar <jharmostafa@gmail.com>
2025-06-14 12:16:46 +0300

    fixed img loading

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,9 @@
-
+import logo from '/src/assets/personal-img.jpg'
 
 export default function Header() {
     return (
         <header className="header">
-            <img className="header-logo" src="/public/personal-img.jpg" />
+            <img className="header-logo" src={logo} />
             <h1>Mustafa Jhar</h1>
             <h2>Frontend Developer</h2>
         </header>
