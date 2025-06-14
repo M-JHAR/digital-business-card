7b9a48443bd55687ccff2d9e7da4ae6f5e575536
Mustafa Jhar <jharmostafa@gmail.com>
2025-06-14 12:03:06 +0300

    updated the main photo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@
 export default function Header() {
     return (
         <header className="header">
-            <img className="header-logo" src="/src/assets/personal-img.jpg" />
+            <img className="header-logo" src="/public/personal-img.jpg" />
             <h1>Mustafa Jhar</h1>
             <h2>Frontend Developer</h2>
         </header>
