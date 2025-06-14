52d3578d1d227f224db46f0fbee9f471a3d80738
Mustafa Jhar <jharmostafa@gmail.com>
2025-06-14 11:58:53 +0300

    finished

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+
+
+export default function Header() {
+    return (
+        <header className="header">
+            <img className="header-logo" src="/src/assets/personal-img.jpg" />
+            <h1>Mustafa Jhar</h1>
+            <h2>Frontend Developer</h2>
+        </header>
+    )
+}
